b24b42728ef684c62585c42064651b1857594e48
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-05-02 15:06:53 -0700

    added work_data

--- pages/works.js
+++ pages/works.js
@@ -3,12 +3,7 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import thumbFloorFans from '../public/images/works/floorfans_eyecatch.png'
-import thumbGamingKeyboards from '../public/images/works/gamingkeyboards_eyecatch.png'
-import thumbAppleSe from '../public/images/works/applewatchsegen2_eyecatch.png'
-import thumbAmazfit from '../public/images/works/amazfit_eyecatch.png'
-import thumbAppleSeries from '../public/images/works/applewatchseries_eyecatch.png'
-import thumbSteelseries from '../public/images/works/steelseries_eyecatch.png'
+import { workData } from "../lib/work_data";
 
 const Works = () => (
   <Layout title="Works">
@@ -18,83 +13,19 @@ const Works = () => (
       </Heading>
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.1}>
-          <WorkGridItem
-            title="Best Floor Fans of 2024"
-            thumbnail={thumbFloorFans}
-            href="https://www.techgearlab.com/topics/home/best-floor-fan"
-          >
-            An article about the 4 best floor fans of 2024
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.1}>
-          <WorkGridItem
-            thumbnail={thumbGamingKeyboards}
-            title="Best Gaming Keyboards"
-            href="https://www.techgearlab.com/topics/cool-gadgets/best-gaming-keyboard"
-          >
-            An article about the 6 best gmaing keyboards
-          </WorkGridItem>
-        </Section>
+        {workData.map((card, index) => (
+          <Section key={index} delay={index * 0.2}>
+            <WorkGridItem
+              thumbnail={card.thumbnail}
+              title={card.title}
+              href={card.href}
+            >
+              {card.description}
+            </WorkGridItem>
+          </Section>
+        ))}
       </SimpleGrid>
 
-      <Section delay={0.2}>
-        <Divider my={6} />
-
-        <Heading as="h3" fontSize={20} mb={4}>
-          Collaborations
-        </Heading>
-      </Section>
-
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.3}>
-          <WorkGridItem
-            thumbnail={thumbAppleSe}
-            title="Apple Watch SE Review"
-            href="https://www.techgearlab.com/reviews/wearable-tech/fitness-tracker/apple-watch-se-gen-2"
-          >
-            A review about the Apple Watch SE Gen 2
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.3}>
-          <WorkGridItem
-            thumbnail={thumbAmazfit}
-            title="Amazfit Band 7 Review"
-            href="https://www.techgearlab.com/reviews/wearable-tech/fitness-tracker/amazfit-band-7"
-          >
-            A review about the Amazfit Band 7
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
-
-      <Section delay={0.4}>
-        <Divider my={6} />
-
-        <Heading as="h3" fontSize={20} mb={4}>
-          Old works
-        </Heading>
-      </Section>
-
-      <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section delay={0.5}>
-          <WorkGridItem
-            thumbnail={thumbAppleSeries}
-            title="Apple Watch Series 8 Review"
-            href="https://www.techgearlab.com/reviews/wearable-tech/smartwatch/apple-watch-series-8"
-          >
-            A review about the Apple Watch Series 8
-          </WorkGridItem>
-        </Section>
-        <Section delay={0.5}>
-          <WorkGridItem
-            thumbnail={thumbSteelseries}
-            title="SteelSeries Apex Pro Review"
-            href="https://www.techgearlab.com/reviews/cool-gadgets/gaming-keyboard/steelseries-apex-pro"
-          >
-            A review about the SteelSeries Apex Pro
-          </WorkGridItem>
-        </Section>
-      </SimpleGrid>
     </Container>
   </Layout>
 )
