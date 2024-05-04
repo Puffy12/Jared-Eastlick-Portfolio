15a24d6a47ada294026fe4ac869ac25524fec4f0
BrianBeilby <brianbeilby98@gmail.com>
2024-05-03 21:40:03 -0700

    Unused

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ import {
 } from '@chakra-ui/react'
 import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
-import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
