d3f7d5c409ea4b027e820934e3db7b216797f0f4
BrianBeilby <brianbeilby98@gmail.com>
2024-05-03 21:36:33 -0700

    Update grid-item.js

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,6 +1,6 @@
 import NextLink from 'next/link'
 import Image from 'next/image'
-import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
+import { Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 import styled from '@emotion/styled';
 
