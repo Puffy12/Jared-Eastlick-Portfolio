f458a7a5c4bdd060f4820b99d3f21a750094c080
BrianBeilby <brianbeilby98@gmail.com>
2024-05-04 19:21:10 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env.local' });
 const nextConfig = {
     reactStrictMode: true,
+
+    env: {
+        SERVICE_ID: process.env.SERVICE_ID,
+        TEMPLATE_ID: process.env.TEMPLATE_ID,
+        USER_ID: process.env.USER_ID
+    }
 };
 
 module.exports = nextConfig;
