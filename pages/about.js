0129e6f0e18dc28042c22fa8dc87d26263d0f86d
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-05-15 21:36:01 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import Layout from '../components/layouts/article';
-import { Container, Heading, Box, Button, List, ListItem, ListIcon, Image } from '@chakra-ui/react';
+import { Container, Heading, Box, Button, List, ListItem, ListIcon, Image, Flex } from '@chakra-ui/react';
 import Paragraph from '../components/paragraph';
 import Section from '../components/section';
 import NextLink from 'next/link';
@@ -10,13 +10,13 @@ import { MdSettings } from "react-icons/md";
 const About = () => {
     return (
         <Layout title="About">
-            <Section delay={0.1} mb={{ base: 16, md: 24 }}>  
+            <Section delay={0.1} mb={{ base: 24, md: 24 }}> 
                 <Container>
                     <Heading
                         as="h3"
                         variant="section-title"
                         textAlign="center"
-                        mb={{ base: 8, md: 12 }}
+                        mb={{ base: 8, md: 12 }}  
                     >
                         About Me
                     </Heading>
@@ -32,7 +32,7 @@ const About = () => {
             <Box
                 bg="gray.200"
                 mt={{ base: 16, md: 24 }}  // Increase top margin for divider
-                mb={{ base: 32, md: 40 }}  // Increase bottom margin for divider
+                mb={{ base: 16, md: 24 }}  // Increase bottom margin for divider
                 h="16"
                 w="1"
                 rounded="full"
@@ -41,58 +41,60 @@ const About = () => {
                 marginX="auto"
                 delay={0.1}
             ></Box>
-            <br/> <br/>
-            <Section delay={0.1} position="relative" mb={{ base: 16, md: 24 }}>  {/* Add margin-bottom for mobile and desktop */}
-                <Box>
+
+            <Section delay={0.1} position="relative" mb={{ base: 24, md: 24 }}>  {/* Increased margin-bottom for mobile */}
+                <Flex
+                    direction={{ base: 'column', md: 'row' }}
+                    align={{ base: 'center', md: 'flex-start' }}
+                >
                     <Image
                         src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                         alt="Green double couch with wooden legs"
                         borderRadius="lg"
                         width="150"
                         height="200"
-                        float="left"
-                        mr={4}
-                        mb={{ base: 8, md: 12 }}  // Increase bottom margin for image
+                        mb={{ base: 4, md: 0 }} // Margin bottom for mobile
+                        mr={{ md: 4 }} // Margin right for desktop
                     />
-                </Box>
-                <Box>
-                    <Heading as="h3" variant="section-title" textAlign="center" mb={{ base: 8, md: 12 }}>  {/* Increase bottom margin for heading */}
-                        More...
-                    </Heading>
-                    <Paragraph mb={{ base: 0, md: 12 }}>  
-                        In my free time, I like to travel, learn new languages, and study. 
-                        I have been learning Korean for nearly 5 years and had the opportunity 
-                        to live in the Korean capital, Seoul, for a year. When I am not studying, 
-                        I enjoy going to the gym and running.
-                    </Paragraph>
-                    <Box align="center" my={{ base: 8, md: 12 }}>  {/* Increase margin for button */}
-                        <Button
-                            as={NextLink}
-                            href="/works"
-                            scroll={false}
-                            rightIcon={<ChevronRightIcon />}
-                            colorScheme="teal"
-                        >
-                            See More
-                        </Button>
+                    <Box>
+                        <Heading as="h3" variant="section-title" textAlign="center" mb={{ base: 4, md: 12 }}>
+                            More...
+                        </Heading>
+                        <Paragraph mb={{ base: 4, md: 12 }}>
+                            In my free time, I like to travel, learn new languages, and study. 
+                            I have been learning Korean for nearly 5 years and had the opportunity 
+                            to live in the Korean capital, Seoul, for a year. When I am not studying, 
+                            I enjoy going to the gym and running.
+                        </Paragraph>
+                        <Box align="center" my={{ base: 8, md: 12 }}>
+                            <Button
+                                as={NextLink}
+                                href="/works"
+                                scroll={false}
+                                rightIcon={<ChevronRightIcon />}
+                                colorScheme="teal"
+                            >
+                                See More
+                            </Button>
+                        </Box>
                     </Box>
-                </Box>
+                </Flex>
             </Section>
 
             <Box
                 bg="gray.200"
                 mt={{ base: 16, md: 24 }}  // Increase top margin for divider
-                mb={{ base: 32, md: 40 }}  // Increase bottom margin for divider
+                mb={{ base: 16, md: 24 }}  // Increase bottom margin for divider
                 h="16"
                 w="1"
                 rounded="full"
                 display={{ base: "none", sm: "block" }}
                 dark={{ bg: "gray.200", bgOpacity: 0.2 }}
                 marginX="auto"
-                delay={0.3}
+                delay={0.1}
             ></Box>
 
-            <Section delay={0.3} mb={{ base: 12, md: 24 }}> 
+            <Section delay={0.3} mb={{ base: 24, md: 24 }}>  
                 <Heading
                     as="h3"
                     variant="section-title"
@@ -124,7 +126,7 @@ const About = () => {
                 </Box>
             </Section>
         </Layout>
-    )
-}
+    );
+};
 
 export default About;
